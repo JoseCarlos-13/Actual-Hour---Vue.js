df7d89e63a22498f9137a52b8280d07f1f0cffa0
José Carlos <josecarlosbsilva.17@gmail.com>
2020-08-17 21:14:00 -0300

    first commit

---
+++ src/components/Landscape/Landscape.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <img class="landscape" :src="actualImage" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'actualImage'
+  ]
+}
+</script>
+
+<style>
+  .landscape{
+    width: 50%;
+    border: 6px solid white;
+    box-shadow: 2px 3px 6px black;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import moment from 'moment'
 
 Vue.config.productionTip = false
 
 new Vue({
+  moment,
   render: h => h(App),
 }).$mount('#app')
